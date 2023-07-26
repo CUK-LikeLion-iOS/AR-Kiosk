29e07a0d779de0a5c2d31184e3cafb5e848151c2
gadisom <jeongwon.kim64@gmail.com>
2023-07-26 12:42:31 +0900

    chore : MVC 구조 적용

---
+++ AR_Kiosk/AR_Kiosk/KioskScreen/Controller/SecondVC.swift
@@ -0,0 +1,19 @@
+//
+//  SecondVC.swift
+//  AR_Kiosk
+//
+//  Created by 김정원 on 2023/07/24.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
